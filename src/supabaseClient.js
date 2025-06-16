b24be5a1e8317159713dfe6bd75e223674a675a0
Garrett Eisenman <garrett@trailbreakpartners.com>
2025-06-16 14:19:43 -0600

    Updated Supabase credentials and setup

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ehqhusqrxdukaynosenb.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVocWh1c3FyeGR1a2F5bm9zZW5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1OTE3NTQsImV4cCI6MjA2NTE2Nzc1NH0.PdzBnhj_jo_6tPihdIlu-lfWGU11TZP0gZD8S7f4nyY'
+const supabaseUrl = 'https://ehqhusqrxdukaynosenb.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVocWh1c3FyeGR1a2F5bm9zZW5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1OTE3NTQsImV4cCI6MjA2NTE2Nzc1NH0.PdzBnhj_jo_6tPihdIlu-lfWGU11TZP0gZD8S7f4nyY';
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
